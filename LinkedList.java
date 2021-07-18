da018465db6e6a69833aa8fb2756575be69dbd79
Ganesh <ganeshgavhad9@gmail.com>
2021-07-18 23:56:37 +0530

    Added : UC2 Pop And Peak Element

--- LinkedList.java
+++ LinkedList.java
@@ -18,12 +18,42 @@ public class LinkedList <T> {
 			node.next=head;
 			head=node;
 		}
+		/**
+		 * For Removing Top Most Value/Data from Stack and queue.
+		 * @return
+		 */
+	
+		public void pop() {
+			if (head == null)
+				System.out.println("\n" + "Stack is Empty!");
+			else {
+				Node<T> temp = head;
+				System.out.printf("Deleted element: %d", head.data);
+				System.out.println();
+				head = temp.next;
+			}
+		}
+		/**
+		 * For Find First Element In Stack
+		 */
+		public T peak() {
+			if(head != null)
+				return head.data;
+			else {
+				System.out.println("Stack is Empty");
+			}
+	  
+			return null;
+		}
 
 		/**
 		 * Printing The Data Of Node till next equal to null
 		 */
 		public void show() {
 			Node<T> node = head;
+			if(head==null) {
+				System.out.println("Stack is Empty");
+			}
 			while (node.next != null) {
 				System.out.println(node.data);
 				node = node.next;
